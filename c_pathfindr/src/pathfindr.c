113c765738d626e8a7d79038a41345288e2c236e
Matt-DESTROYER <matthewsalgorithm@gmail.com>
2025-05-05 10:06:40 +1000

    Finally working lol

--- c_pathfindr/src/pathfindr.c
+++ c_pathfindr/src/pathfindr.c
@@ -19,52 +19,37 @@ void adjacent_directions(maze_t* maze, uint_t x, uint_t y) {
 	}
 }
 */
-void recurse_adjacents(maze_t* maze, uint_t x, uint_t y, int x_dir, int y_dir) {
-	if (x_dir > 0) {
-		if ((x > 0) && (maze_at(maze, x - 1, y) != MAZE_BLOCK)) {
-			maze_set(maze, PATH_RIGHT, x - 1, y);
-			recurse_adjacents(maze, x - 1, y, x_dir, y_dir);
-		}
-		if ((x < (maze->width - 1)) && (maze_at(maze, x + 1, y) != MAZE_BLOCK)) {
-			maze_set(maze, PATH_LEFT, x + 1, y);
-			recurse_adjacents(maze, x + 1, y, x_dir, y_dir);
-		}
-	} else {
-		if ((x < (maze->width - 1)) && (maze_at(maze, x + 1, y) != MAZE_BLOCK)) {
-			maze_set(maze, PATH_LEFT, x + 1, y);
-			recurse_adjacents(maze, x + 1, y, x_dir, y_dir);
-		}
-		if ((x > 0) && (maze_at(maze, x - 1, y) != MAZE_BLOCK)) {
-			maze_set(maze, PATH_RIGHT, x - 1, y);
-			recurse_adjacents(maze, x - 1, y, x_dir, y_dir);
-		}
+bool unexplored_cell(maze_t* maze, uint_t x, uint_t y) {
+	char cell = maze_at(maze, x, y);
+	return (
+		(cell == MAZE_EMPTY) ||
+		(cell == MAZE_START) ||
+		(cell == MAZE_END)
+	);
+}
+void explore_cell(maze_t* maze, char value, uint_t x, uint_t y) {
+	maze_set(maze, value, x, y);
+	recurse_adjacents(maze, x, y);
+}
+void recurse_adjacents(maze_t* maze, uint_t x, uint_t y) {
+	if ((y > 0) && (unexplored_cell(maze, x, y - 1))) {
+		explore_cell(maze, PATH_DOWN, x, y - 1);
 	}
-	if (y_dir > 0) {
-		if ((y > 0) && (maze_at(maze, x, y - 1) != MAZE_BLOCK)) {
-			maze_set(maze, PATH_DOWN, x, y - 1);
-			recurse_adjacents(maze, x, y - 1, x_dir, y_dir);
-		}
-		if ((y < (maze->height - 1)) && (maze_at(maze, x, y + 1) != MAZE_BLOCK)) {
-			maze_set(maze, PATH_UP, x, y + 1);
-			recurse_adjacents(maze, x, y + 1, x_dir, y_dir);
-		}
-	} else {
-		if ((y < (maze->height - 1)) && (maze_at(maze, x, y + 1) != MAZE_BLOCK)) {
-			maze_set(maze, PATH_UP, x, y + 1);
-			recurse_adjacents(maze, x, y + 1, x_dir, y_dir);
-		}
-		if ((y > 0) && (maze_at(maze, x, y - 1) != MAZE_BLOCK)) {
-			maze_set(maze, PATH_DOWN, x, y - 1);
-			recurse_adjacents(maze, x, y - 1, x_dir, y_dir);
-		}
+	if ((x > 0) && (unexplored_cell(maze, x - 1, y))) {
+		explore_cell(maze, PATH_RIGHT, x - 1, y);
+	}
+	if ((y < (maze->height - 1)) && (unexplored_cell(maze, x, y + 1))) {
+		explore_cell(maze, PATH_UP, x, y + 1);
+	}
+	if ((x < (maze->width - 1)) && (unexplored_cell(maze, x + 1, y))) {
+		explore_cell(maze, PATH_LEFT, x + 1, y);
 	}
 }
 
 void find_path(maze_t* maze, int start_x, int start_y, int end_x, int end_y) {
-	int x_dir = end_x - start_x,
-		y_dir = end_y - start_y;
+	//int x_dir = end_x - start_x, y_dir = end_y - start_y;
 
-	recurse_adjacents(maze, (uint_t)end_x, (uint_t)end_y, x_dir, y_dir);
+	recurse_adjacents(maze, (uint_t)end_x, (uint_t)end_y);
 }
 
 void next_position(maze_t* maze, uint_t* x, uint_t* y) {
@@ -102,15 +87,16 @@ int maze_find_path(maze_t* input_maze, maze_t* output_maze) {
 	// find a path
 	find_path(output_maze, start_x, start_y, end_x, end_y);
 
-	maze_print(output_maze);
-
 	// trace path
-	uint_t x = (uint_t)start_x, y = (uint_t)start_y;
-	next_position(output_maze, &x, &y);
-	while ((x != end_x) && (y != end_y)) {
-		maze_set(output_maze, MAZE_PATH, x, y);
-		next_position(output_maze, &x, &y);
-		maze_print(output_maze);
+	if (maze_at(output_maze, (uint_t)start_x, (uint_t)start_y) != MAZE_START) {
+		uint_t curr_x = (uint_t)start_x, curr_y = (uint_t)start_y,
+		       next_x = curr_x, next_y = curr_y;
+		while ((curr_x != end_x) || (curr_y != end_y)) {
+			next_position(output_maze, &next_x, &next_y);
+			maze_set(output_maze, MAZE_PATH, curr_x, curr_y);
+			curr_x = next_x;
+			curr_y = next_y;
+		}
 	}
 
 	for (uint_t y = 0; y < output_maze->height; y++) {
@@ -120,11 +106,15 @@ int maze_find_path(maze_t* input_maze, maze_t* output_maze) {
 				case PATH_RIGHT:
 				case PATH_UP:
 				case PATH_DOWN:
-					maze_set(output_maze, MAZE_EMPTY, x, y);
+					maze_set(output_maze, MAZE_VISITED, x, y);
 					break;
 			}
 		}
 	}
+
+	// cleanup
+	maze_set(output_maze, MAZE_START, (uint_t)start_x, (uint_t)start_y);
+	maze_set(output_maze, MAZE_END, (uint_t)end_x, (uint_t)end_y);
 
 	printf("[OUTPUT MAZE]\n");
 	maze_print(output_maze);
--- c_pathfindr/src/pathfindr.h
+++ c_pathfindr/src/pathfindr.h
@@ -11,6 +11,15 @@
 #define PATH_LEFT  'L'
 #define PATH_RIGHT 'R'
 
+typedef int bool;
+#define true 1
+#define false 0
+
+bool unexplored_cell(maze_t* maze, uint_t x, uint_t y);
+void explore_cell(maze_t* maze, char value, uint_t x, uint_t y);
+void recurse_adjacents(maze_t* maze, uint_t x, uint_t y);
+void find_path(maze_t* maze, int start_x, int start_y, int end_x, int end_y);
+void next_position(maze_t* maze, uint_t* x, uint_t* y);
 int maze_find_path(maze_t* input_maze, maze_t* output_maze);
 
 #endif
